8d2a609f5026b085f3b44ac10d07462a3228526d
Michael Dudley <sound.plus.fury@gmail.com>
2020-04-10 14:34:04 -0700

    Added employee detail component

---
+++ force-app/main/default/lwc/employeeDetail/employeeDetail.js
@@ -0,0 +1,9 @@
+import { LightningElement, api } from 'lwc';
+
+export default class EmployeeDetail extends LightningElement {
+    @api empDetail={empName:"John", empAddress:"TX"}
+
+
+
+
+}
---
+++ force-app/main/default/lwc/employeeDetails/employeeDetails.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class EmployeeDetails extends LightningElement {
+    empDetails=[
+        {empName: "Steve", empAddress: 'NC'},
+        {empName: "Tom", empAddress: 'The Torlet'},
+        {empName: "Dick", empAddress: 'Butts'},
+        {empName: "Harry", empAddress: 'Coochville'},
+        {empName: "Other Steve", empAddress: 'Dongs'},
+
+    ]
+}

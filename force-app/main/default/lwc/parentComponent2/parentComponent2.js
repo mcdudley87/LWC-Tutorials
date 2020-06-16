a1a4c85fa21f79fd9c5136b5ff8c63b0156432ac
Michael Dudley <sound.plus.fury@gmail.com>
2020-06-16 15:34:53 -0700

    new components for Udemy demos

--- force-app/main/default/lwc/parentComponent2/parentComponent2.js
+++ force-app/main/default/lwc/parentComponent2/parentComponent2.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc';
 
-export default class ParentComponent2 extends LightningElement {}
+export default class ParentComponent2 extends LightningElement {
+    @track msg;
+   constructor()
+   {
+       super();
+       this.template.addEventListener('mycustomevent',this.handleCustomEvent.bind(this));
+   }
+    handleCustomEvent(event)
+    {
+        this.msg=event.detail;
+    }
+}
